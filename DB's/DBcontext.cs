fb25d6b3994be352362b4fa043ed160ed71b4ad6
Luis Abreu <2241872@my.ipleiria.pt>
2025-05-29 15:52:22 +0100

    primeiro pull

---
+++ DB's/DBcontext.cs
@@ -0,0 +1,25 @@
+﻿using iTasks.CLASES;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static iTasks.CLASES.utilizador;
+
+
+
+
+
+namespace iTasks.DB_s
+{
+    class BDContext : DbContext
+    {
+        public DbSet<Utilizador> Utilizadores { get; set; }
+        public DbSet<Gestor> Gestores { get; set; }
+        public DbSet<Programador> Programadores { get; set; }
+
+
+    }
+}
---
+++ frmConsultarTarefasConcluidas.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace iTasks
+{
+    partial class frmConsultarTarefasConcluidas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btFechar = new System.Windows.Forms.Button();
+            this.gvTarefasConcluidas = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gvTarefasConcluidas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btFechar
+            // 
+            this.btFechar.Location = new System.Drawing.Point(1245, 511);
+            this.btFechar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btFechar.Name = "btFechar";
+            this.btFechar.Size = new System.Drawing.Size(139, 28);
+            this.btFechar.TabIndex = 32;
+            this.btFechar.Text = "Fechar";
+            this.btFechar.UseVisualStyleBackColor = true;
+            // 
+            // gvTarefasConcluidas
+            // 
+            this.gvTarefasConcluidas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvTarefasConcluidas.Location = new System.Drawing.Point(16, 15);
+            this.gvTarefasConcluidas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gvTarefasConcluidas.Name = "gvTarefasConcluidas";
+            this.gvTarefasConcluidas.RowHeadersWidth = 51;
+            this.gvTarefasConcluidas.Size = new System.Drawing.Size(1368, 486);
+            this.gvTarefasConcluidas.TabIndex = 31;
+            // 
+            // frmConsultarTarefasConcluidas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1403, 554);
+            this.Controls.Add(this.btFechar);
+            this.Controls.Add(this.gvTarefasConcluidas);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmConsultarTarefasConcluidas";
+            this.Text = "frmConsultarTarefasConcluidas";
+            ((System.ComponentModel.ISupportInitialize)(this.gvTarefasConcluidas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btFechar;
+        private System.Windows.Forms.DataGridView gvTarefasConcluidas;
+    }
+}
